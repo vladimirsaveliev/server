dcfa738b507555fe4e5613fb67c7d729a2899788
Vladimir Saveliev <c17830@cray.com>
2021-02-14 16:42:26 +0300

    application.properties in test

--- src/test/java/cz/cvut/fit/tjv/semproject/server/ServerApplicationTests.java
+++ src/test/java/cz/cvut/fit/tjv/semproject/server/ServerApplicationTests.java
@@ -1,9 +1,11 @@
 package cz.cvut.fit.tjv.semproject.server;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ServerApplication.class)
+@AutoConfigureTestDatabase
 class ServerApplicationTests {
     @Test
     void test1() {
